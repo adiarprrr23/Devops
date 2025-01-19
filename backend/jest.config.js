096e2b4e8acf91ebbedcc93e867d2e2088bb078a
P Aditya Rao <107695133+adiarprrr23@users.noreply.github.com>
2025-01-19 13:46:35 +0530

    Update jest.config.js

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,7 @@
-// jest.config.js (ES Module syntax)
 export default {
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"],
-  transform: {},
+  transform: {
+    "^.+\\.js$": "babel-jest",
+  },
 };
